d7246aee3e7601750c8a7518c2c67c53f7c37503
visheshgupta0404 <visheshg990@gmail.com>
2026-04-23 22:53:50 +0530

    Update ItemDetailActivity.java

---
+++ app/src/main/java/com/example/borrowme/ui/items/ItemDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.borrowme.ui.items;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.borrowme.R;
+
+public class ItemDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
